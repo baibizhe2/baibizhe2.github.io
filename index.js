3817721c849ceb91b50d8202e616009042ea4872
baibizhe2 <113849210+baibizhe2@users.noreply.github.com>
2022-09-19 00:11:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello, github pages :)'
+  document.getElementById('main-content').innerHTML = 'Hello,ADDDD.FCCCCC  WELCOME TO OUR PAGE . WE ARE THE NO.1 IN THE UNIVERSE :)'
 }
