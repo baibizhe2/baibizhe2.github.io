3a46c12b53461f37abd56fabbaaea51fba7146b0
baibizhe2 <113849210+baibizhe2@users.noreply.github.com>
2022-09-19 09:35:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 window.onload = function() {
-  document.getElementById('main-content').innerHTML = 'Hello,ADDDD.FCCCCC  WELCOME TO OUR PAGE . WE ARE THE NO.1 IN THE UNIVERSE :) <img src= "team_photo.jpg" />' 
+  document.getElementById('main-content').innerHTML = 'Hello,ADDDD.FCCCCC  WELCOME TO OUR PAGE . WE ARE THE NO.1 IN THE UNIVERSE :) ' 
 }
